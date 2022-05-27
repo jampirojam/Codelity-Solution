852e49fe353af3cadbd5d412b5d81d2a3b1156aa
M. Machrush Aliy Sirojjam Mushlich <jampirojam@gmail.com>
2022-05-27 22:17:54 +0700

    Update Example Test.java

--- Java/Example Test.java
+++ Java/Example Test.java
@@ -1,28 +1,28 @@
 // you can also use imports, for example:
 // import java.util.*;
-
+import java.util.*;
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
 
 class Solution {
-    public int solution(int[] A) {
-        // write your code in Java SE 8
-        int min = 1;
-        int max = 100000;
-        boolean[] value = new boolean[max+1];
-        if (A.length <= 0) return min;
-
-        for (int i = 0; i < A.length; i++){
-            if (A[i] < max+1) value[A[i]] = true;
-        }
-        for (int i = 1; i < max; i++){
-            if (value[i] && min == i){
-                min++;
-            } else if (!value[i]){
-                break;
+  public int solution(int[] A) {
+    // write your code in Java SE 8
+    Set<Integer> set=new TreeSet<>();
+        for (int x:A) {
+            if (x>0) {
+                set.add(x);
             }
         }
-        if (min > max) return max;
-        return min;
-    }
+
+        int y=1;
+        Iterator<Integer> it=set.iterator();
+        while (it.hasNext()) {
+            int curr=it.next();
+            if (curr!=y) {
+                return y;
+            }
+            y++;
+        }
+        return y;
+  }
 }
